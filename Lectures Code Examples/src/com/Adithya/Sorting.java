5846a8f8b0d80a7fae28a0f4bbed44e4b5eb5c05
Arla-Adithya <f20190646@goa.bits-pilani.ac.in>
2021-09-04 20:54:38 +0530

    Added Few Changes

--- Lectures Code Examples/src/com/Adithya/Sorting.java
+++ Lectures Code Examples/src/com/Adithya/Sorting.java
@@ -6,8 +6,22 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         int[] arr = {5,4,3,2,1};
-        SelectionSort(arr);
+        InsertionSort(arr);
         System.out.println(Arrays.toString(arr));
+    }
+    static void InsertionSort(int[] arr) {
+        for (int i=0;i<arr.length-1;i++) {
+            for (int j = i+1; j >0 ; j--) {
+                if (arr[j]<arr[j-1]) {
+                    swap(arr,j,j-1);
+                }
+                else {
+                    break;
+                }
+
+            }
+
+        }
     }
     static void SelectionSort(int[] arr) {
         for (int i = 0; i <arr.length ; i++) {
@@ -33,6 +47,7 @@ public class Sorting {
             }
         }
     }
+
     static int maxIndex(int[] arr, int start, int end) {
         int max= start;
         for (int i = start; i <=end ; i++) {
