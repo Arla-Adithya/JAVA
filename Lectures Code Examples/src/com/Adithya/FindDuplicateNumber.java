7bbd3651a12be2bed0d12644f82bc778c0252b71
Arla-Adithya <f20190646@goa.bits-pilani.ac.in>
2021-09-05 12:05:03 +0530

    Added a Google and Amazon  Question

---
+++ Lectures Code Examples/src/com/Adithya/FindDuplicateNumber.java
@@ -0,0 +1,33 @@
+package com.Adithya;
+// Amazon Question
+public class FindDuplicateNumber {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,2,2};
+        System.out.println(findDuplicateNumber(arr));
+    }
+    static int findDuplicateNumber(int[] arr) {
+        int i = 0;
+        while(i<arr.length) {
+            int correct  = arr[i] - 1;
+            if (arr[i] != i+1) {
+                if(arr[i] != arr[correct]) {
+                    swap(arr,i,correct);
+                }
+                else {
+                    return arr[i];
+                }
+            }
+            else {
+                i++;
+            }
+        }
+
+        return -1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
